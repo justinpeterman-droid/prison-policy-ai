a17c9d5003b8b56ef5efdefe9c1086d6c6c69d5c
justinpeterman-droid <justinpeterman@hometownserenity.com>
2026-08-18 23:42:15 -0500

    feat: export incident packet services

--- backend/forms/__init__.py
+++ backend/forms/__init__.py
@@ -6,10 +6,36 @@ from backend.forms.catalog import (
     load_form_catalog,
     sync_form_catalog,
 )
+from backend.forms.packets import (
+    IncidentPacketItemView,
+    PacketConfigurationError,
+    PacketMutationNotAllowed,
+    PacketNotFound,
+    PacketRevisionConflict,
+    PlannedPacketItem,
+    add_additional_form,
+    build_incident_packet,
+    list_incident_packet,
+    mark_not_applicable,
+    plan_incident_packet,
+    remove_recommended_form,
+)
 
 __all__ = [
     "CatalogConfigurationError",
     "FormTemplateDefinition",
+    "IncidentPacketItemView",
+    "PacketConfigurationError",
+    "PacketMutationNotAllowed",
+    "PacketNotFound",
+    "PacketRevisionConflict",
+    "PlannedPacketItem",
+    "add_additional_form",
+    "build_incident_packet",
+    "list_incident_packet",
     "load_form_catalog",
+    "mark_not_applicable",
+    "plan_incident_packet",
+    "remove_recommended_form",
     "sync_form_catalog",
 ]
